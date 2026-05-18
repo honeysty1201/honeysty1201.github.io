4f038287f85e42d61594963f2274e006e6b97d45
CHOI HYUNSEOK <irritableson5@gmail.com>
2026-05-18 16:16:37 +0900

    pushed base astro_1

--- script.js
+++
@@ -1,6 +0,0 @@
-const button = document.getElementById("change-btn");
-const text = document.getElementById("hello-text");
-
-button.addEventListener("click", () => {
-  text.textContent = "GitHub Pages Works!";
-});
---
+++ src/site.config.ts
@@ -0,0 +1,83 @@
+export const SITE_SETTINGS = {
+  title: "Daniel Adrian's site",
+  description: "A space to explore ideas and share what I'm learning",
+  owner: "Daniel Adrian",
+  ogImages:
+    "https://res.cloudinary.com/dellp9a4z/image/upload/v1756758385/og-home_ejuqq8.png",
+  socials: [
+    {
+      icon: "github",
+      label: "GitHub",
+      url: "https://github.com/truedaniyyel",
+      handle: "truedaniyyel",
+    },
+  ],
+};
+
+export const header = [
+  {
+    name: "Blog",
+    url: "/blog",
+  },
+  {
+    name: "Projects",
+    url: "/projects",
+  },
+  {
+    name: "About",
+    url: "/about",
+  },
+  {
+    name: "Contact",
+    url: "/contact",
+  },
+  {
+    name: "Newsletter",
+    url: "/newsletter",
+  },
+];
+
+export const footer = [
+  {
+    title: "Content",
+    links: [
+      {
+        name: "Search",
+        url: "/search",
+      },
+      {
+        name: "Blog",
+        url: "/blog",
+      },
+      {
+        name: "Projects",
+        url: "/projects",
+      },
+      {
+        name: "About",
+        url: "/about",
+      },
+    ],
+  },
+  {
+    title: "Resources",
+    links: [
+      {
+        name: "Contact",
+        url: "/contact",
+      },
+      {
+        name: "Newsletter",
+        url: "/newsletter",
+      },
+      {
+        name: "RSS",
+        url: "/rss.xml",
+      },
+      {
+        name: "Sitemap",
+        url: "/sitemap-index.xml",
+      },
+    ],
+  },
+];
